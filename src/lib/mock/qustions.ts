034da0bfe62ac93e82e33d621ff71e148aac56ba
Andyxdddd <badea.ulrich@outlook.com>
2024-11-30 12:44:46 +0100

    feat: add mock questions

---
+++ src/lib/mock/qustions.ts
@@ -0,0 +1,16 @@
+import { Question } from "../types";
+
+export default [
+  {
+    id: "1",
+    question: "What is your name?",
+  },
+  {
+    id: "2",
+    question: "What is your email?",
+  },
+  {
+    id: "3",
+    question: "What is your age?",
+  },
+] satisfies Question[];
