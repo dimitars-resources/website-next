e4c47d37cbacb489c8de51d8f4d31d8bca41fe30
Andyxdddd <badea.ulrich@outlook.com>
2024-11-30 12:59:02 +0100

    feat: integrate mockQuestions into WhitelistForm for dynamic question rendering

--- src/app/(pages)/whitelist-form/page.tsx
+++ src/app/(pages)/whitelist-form/page.tsx
@@ -1,3 +1,4 @@
+import { mockQuestions } from "@/lib/mock";
 import React from "react";
 
 const WhitelistForm = () => {
@@ -6,14 +7,12 @@ const WhitelistForm = () => {
       <section className="flex flex-col items-center space-y-4 pt-20">
         <h1 className="text-5xl font-bold text-white">Whitelist Form</h1>
         <form className="w-full max-w-xl space-y-8 pt-10">
-          <div>
-            <label className="text-white">FiveM Username</label>
-            <input type="text" className="w-full rounded bg-zinc-900 p-2" placeholder="FiveM Username" />
-          </div>
-          <div>
-            <label className="text-white">Discord Username</label>
-            <input type="text" className="w-full rounded bg-zinc-900 p-2" placeholder="Discord Username" />
-          </div>
+          {mockQuestions.map((question) => (
+            <div key={question.id}>
+              <label className="text-white">{question.question}</label>
+              <input type="text" className="w-full rounded bg-zinc-900 p-2" />
+            </div>
+          ))}
 
           <div className="flex justify-end">
             <button className="bg-blue-800 p-2 px-4">Submit</button>
