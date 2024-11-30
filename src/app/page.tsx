9c482737922f871332761e637c61b27b0891f571
Andyxdddd <badea.ulrich@outlook.com>
2024-11-30 03:52:26 +0100

    chore: format code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="space-y-44 my-44">
-      <section className="space-y-8  text-center flex items-center flex-col">
+    <div className="my-44 space-y-44">
+      <section className="flex flex-col items-center space-y-8 text-center">
         <h1 className="text-7xl font-bold">Server Name</h1>
         <p className="w-full max-w-2xl">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti,
@@ -13,17 +13,20 @@ export default function Home() {
           rerum totam reiciendis saepe, repellat excepturi dignissimos omnis
           expedita dicta reprehenderit laborum.
         </p>
-        <Link href="/whitelist-form" className="p-2 px-4 bg-blue-800">
+        <Link
+          href="/whitelist-form"
+          className="rounded-sm bg-blue-800 px-4 py-2"
+        >
           Apply Now!
         </Link>
       </section>
 
-      <section className="space-y-4  flex flex-col">
+      <section className="flex flex-col space-y-4">
         <h1 className="text-xl font-bold text-white">Sponsors</h1>
         <Sponsors />
       </section>
 
-      <section className="space-y-4  flex flex-col">
+      <section className="flex flex-col space-y-4">
         <h1 className="text-xl font-bold text-white">Live Now</h1>
         <Streams />
       </section>
