e320181763a0ae0e21ae1da8e5ad2bea64dfa5b5
Andyxdddd <badea.ulrich@outlook.com>
2024-12-01 11:45:35 +0100

    feat(header): update header to include user session and conditional logout button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,26 @@
 import Link from "next/link";
 import Button from "./ui/button";
+import { auth } from "@/lib/auth";
 import { signOutAction } from "@/lib/actions";
 
-const Header = () => {
+const Header = async () => {
+  const session = await auth();
+
   return (
     <header className="h-12">
-      <nav className="flex h-full items-center space-x-4 *:text-xl">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
+      <nav className="flex h-full items-center justify-between *:space-x-4">
+        <div className="*:text-xl">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+        </div>
 
-        <form action={signOutAction}>
-          <Button type="submit">Log Out</Button>
-        </form>
+        {session?.user && (
+          <form action={signOutAction}>
+            <Button type="submit" size="small">
+              Log Out
+            </Button>
+          </form>
+        )}
       </nav>
     </header>
   );
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import Discord from "next-auth/providers/discord";
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
+export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Discord({
       authorization: "https://discord.com/api/oauth2/authorize?scope=identify+email+guilds",
