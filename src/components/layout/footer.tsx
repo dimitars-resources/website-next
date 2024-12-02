1dabb70f09ed9e47147195b1956ae40d63d37083
Andyxdddd <badea.ulrich@outlook.com>
2024-12-02 07:27:18 +0100

    style: update global styles and footer background; refactor header links into an array

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="h-96 border-t border-white/5 bg-zinc-900/50">
+    <footer className="h-96 border-t border-white/5">
       <nav className="mx-auto h-full max-w-7xl p-4">Footer</nav>
     </footer>
   );
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,16 +4,24 @@ import Button from "../ui/button";
 import { auth } from "@/lib/auth";
 import { signOutAction } from "@/lib/actions";
 
+const tabs = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Dashboard", href: "/dashboard" },
+];
+
 const Header = async () => {
   const session = await auth();
 
   return (
     <header className="fixed top-0 z-10 h-16 w-full border-b border-white/5 bg-background">
       <nav className="mx-auto flex h-full max-w-7xl items-center justify-between px-4">
-        <div className="space-x-4 *:text-xl">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/dashboard">Dashboard</Link>
+        <div className="space-x-6">
+          {tabs.map(({ name, href }) => (
+            <Link key={name} href={href} className="underline-offset-8 hover:underline">
+              {name}
+            </Link>
+          ))}
         </div>
 
         {session?.user && (
