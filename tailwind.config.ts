ebc91c441bfebe2c597333e1a96b28ac5292783e
Andyxdddd <badea.ulrich@outlook.com>
2024-12-05 14:53:44 +0100

    feat(popover): implement Popover component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
         primary: "#7285cf",
+        popover: "#1a1a1a",
       },
     },
   },
